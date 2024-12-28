0e8a294a67a775684f103ef9fc38a0a3c04561ee
Mohidworldwide <mohidssharif@gmail.com>
2024-12-28 21:35:46 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+# ###activity 1
+
+
+# # def printnumber(n):
+# #     magicalamount = 17
+# #     print("the total number enterent my user is",n)
+# #     magicalamount+=900
+# #     print("total magic done by you in",magicalamount,"\n")
+
+# # printnumber(12)
+# # printnumber(201)
+# # print("/n with any 'n' the dust taken by you will not change")
+
+####activity2
+# def OnTime(n):
+#     iteration=0
+#     for i in range(1,n+1)
+#         iteration+=1
+#     print("When n is ",n," Iterations = ",iteration)
+
+# OnTime(10)
+# OnTime(20)
+# OnTime(42)
+
+# print("\nWith every 'n' the time taken and iterations will increase linearly")
+
+def test(n):
+    iteration = 0
+    for i in range(8,n):
+        for j in range(8,n):
+            print("*",end="")
+            iteration+=1
+        print("")
+    print("/nwhen n is",n,"iteration =",iteration)
+test(5)
+test(4)
+test(3)
+
+print("\n with every 'n' the taken = n^2")
+print("(n^2)")
